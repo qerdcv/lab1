7d436753744a8e3b192897bdad92818ba1762145
qerdcv <vadym.tishchenko2@gmail.com>
2023-05-04 09:52:35 +0300

    Update s3 backend tf script

--- tf_s3_backend/main.tf
+++ tf_s3_backend/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket         = "example-terraform-state-bucket"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "example-terraform-locks"
-    encrypt        = true
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
@@ -20,14 +10,6 @@ resource "aws_s3_bucket" "terraform_state" {
     enabled = true
   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
   tags = {
     Terraform   = "true"
     Environment = "dev"
@@ -36,19 +18,24 @@ resource "aws_s3_bucket" "terraform_state" {
 
 resource "aws_dynamodb_table" "terraform_locks" {
   name           = "example-terraform-locks"
-  billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "LockID"
   attribute {
     name = "LockID"
-    type = "S"
-  }
-  attribute {
-    name = "CreateDate"
     type = "S"
   }
 
   tags = {
     Terraform   = "true"
     Environment = "dev"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "example-terraform-state-bucket"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "example-terraform-locks"
+    encrypt        = true
   }
 }
